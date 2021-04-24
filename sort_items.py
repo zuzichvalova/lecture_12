c2d13f60560dede9befe44dac3d13be42706f63f
Vratislav Č <78488029+vrcmi@users.noreply.github.com>
2021-04-24 09:57:00 +0200

    Create sort_items.py

---
+++ sort_items.py
@@ -0,0 +1,59 @@
+import csv
+import os
+import random
+
+cwd_path = os.getcwd()
+
+def read_row(file_name):
+    """
+    Reads one row for a CSV file and returns numeric data.
+    :param file_name: (str), name of CSV file
+    :return: (list, int),
+    """
+
+
+def read_rows(file_name, row_number):
+    """
+    Reads selected row for a CSV file and returns selected numeric data.
+    :param file_name: (str), name of CSV file
+    :param row_number: (int), number of selected row
+    :return: (list, int),
+    """
+
+
+def selection_sort(number_array):
+    """
+        Sorts and returns selected numeric data with Selection Sort.
+        :param number_array: (list,int), list with numeric array
+        :return: (list, int), sorted numeric array
+    """
+
+
+def bubble_sort(number_array):
+    """
+       Sorts and returns selected numeric data with Bubble Sort.
+       :param number_array: (list,int), list with numeric array
+       :return: (list, int), sorted numeric array
+    """
+
+
+def main():
+
+    # Ukol: Selection Sort
+
+
+    # Ukol: Selection Sort - se smerem razeni
+    
+
+    # Ukol: Bubble Sort
+
+
+    # příklad výpisu hodnot seřazené řady
+    # print ("Seřazená řada čísel je:")
+    # for i in range(len(number_array)):
+    #	print ("%d" %number_array[i]),
+
+
+if __name__ == '__main__':
+    main()
+
