f83c4640251ae38a2d11d21a825e65e318f1e515
zuzichvalova <zuzka.chvalova@gmail.com>
2021-04-26 10:26:32 +0200

    Selection sort

--- sort_items.py
+++ sort_items.py
@@ -4,12 +4,22 @@ import random
 
 cwd_path = os.getcwd()
 
+
 def read_row(file_name):
     """
     Reads one row for a CSV file and returns numeric data.
     :param file_name: (str), name of CSV file
     :return: (list, int),
     """
+    # file_path = os.path.join(cwd_path, file_name)
+
+    with open(file_name, "r",) as f:
+        reader = csv.reader(f, delimiter="\t")
+
+        for row in reader:
+            list_of_num = [int(num) for num in row]
+
+        return list_of_num
 
 
 def read_rows(file_name, row_number):
@@ -27,6 +37,17 @@ def selection_sort(number_array):
         :param number_array: (list,int), list with numeric array
         :return: (list, int), sorted numeric array
     """
+    # hlavny cyklus prechadzania sekvencie
+
+    for count, _ in enumerate(number_array):
+        min_idx = count
+        for num_idx, number in enumerate(number_array[count:]):
+            if number < number_array[min_idx]:
+                min_idx = num_idx + count #priebezne minimum
+
+        number_array[count], number_array[min_idx] = number_array[min_idx], number_array[count]
+
+    return number_array
 
 
 def bubble_sort(number_array):
@@ -39,8 +60,11 @@ def bubble_sort(number_array):
 
 def main():
 
-    # Ukol: Selection Sort
+    row = read_row("numbers_one.csv")
 
+    # Ukol: Selection Sort
+    sorter_row = selection_sort(row)
+    print(sorter_row)
 
     # Ukol: Selection Sort - se smerem razeni
     
